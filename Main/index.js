cab3737750ff6035cb5b157229f38cf6ac2f6293
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-12-24 10:36:27 -0600

    first commit

--- Main/index.js
+++ Main/index.js
@@ -1,18 +1,18 @@
-import { LinkedList } from "../class/LinkedList.js"
+// import { LinkedList } from "../class/LinkedList.js"
 
-// example uses class syntax - adjust as necessary
-const list = new LinkedList();
+// // example uses class syntax - adjust as necessary
+// const list = new LinkedList();
 
-list.append("dog");
-list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
-list.prepend("Mewtwo")
+// list.append("dog");
+// list.append("cat");
+// list.append("parrot");
+// list.append("hamster");
+// list.append("snake");
+// list.append("turtle");
+// list.prepend("Mewtwo")
 
 
-console.log(list.toString())
+// console.log(list.toString())
 
-console.log(list.size)
+// console.log(list.size)
 
